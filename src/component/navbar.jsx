9cf8cca6756d70710a2212f22f1c930b109f0fb5
Madhav <65571082+kvsaimadhav@users.noreply.github.com>
2024-10-28 14:28:24 +0530

    Update navbar.jsx v1.0.3

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -21,19 +21,19 @@ const navbar = () => {
                 <li><a href="#contact" rel="no-opener">Contact</a></li>
                 <li><span>Documents</span>
                     <ul class="doc">
-                        <li><a href={resume} rel="no-referrer" target="_blank">Resume</a></li>
-                        <li><a href={cv} rel="no-referrer" target="_blank">CV</a></li>
-                        <li><a href={cl} rel="no-referrer" target="_blank">Letter</a></li>
+                        <li><a href={resume} rel="noreferrer" target="_blank">Resume</a></li>
+                        <li><a href={cv} rel="noreferrer" target="_blank">CV</a></li>
+                        <li><a href={cl} rel="noreferrer" target="_blank">Letter</a></li>
                         <li><span><img class="cmich" src={cmich} alt="Certifications achieved in CMich"/><br/>&nbsp;Certification</span>
                             <ul class="cert">
-                                <li><a href={aws} rel="no-referrer" target="_blank">&nbsp;AWS</a></li>
-                                <li><a href={oracle} rel="no-referrer" target="_blank">&nbsp;Oracle</a></li>
+                                <li><a href={aws} rel="noreferrer" target="_blank">&nbsp;AWS</a></li>
+                                <li><a href={oracle} rel="noreferrer" target="_blank">&nbsp;Oracle</a></li>
                             </ul>
                         </li>
                         <li><span><img class="cmich" src={bits} alt="BITS Pilani attained Medals"/><br/>&nbsp;Above Beyond</span>
                             <ul class="extrac">
-                                <li><a href={bosm} rel="no-referrer" target="_blank">Sports</a></li>
-                                <li><a href={coep} rel="no-referrer" target="_blank">Nationals</a></li>
+                                <li><a href={bosm} rel="noreferrer" target="_blank">Sports</a></li>
+                                <li><a href={coep} rel="noreferrer" target="_blank">Nationals</a></li>
                             </ul>
                         </li>
                     </ul>
