ced71327526a9ef6eff590e2ba57b5c124cb0768
Madhav <65571082+kvsaimadhav@users.noreply.github.com>
2024-10-28 13:58:24 +0530

    Update home.jsx v1.0.2

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -14,7 +14,7 @@ const home = () =>{
           <p>
             <b>Hello</b>, <i>Everyone</i> .... <br/>
             <i>I'm a Software Engineer</i>specialized in <b>Web Designer &#38; Developer</b> .... <br/>
-            You can reach out to me via <a href="https://www.linkedin.com/in/smadhavkv96/" rel="no-opener" target="_blank">LinkedIn</a> 
+            You can reach out to me via <a href="https://www.linkedin.com/in/smadhavkv96/" rel="no-referrer no-opener" target="_blank">LinkedIn</a> 
             &nbsp; <i>or</i> &nbsp; <a href="https://github.com/kvsaimadhav/" rel="no-referrer no-opener" target="_blank">GitHub</a>
           </p>
         </td>
